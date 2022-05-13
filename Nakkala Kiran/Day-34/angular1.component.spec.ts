09cba3ababbf2ea812e110746836537e36599e7c
kirankumar366 <102592068+kirankumar366@users.noreply.github.com>
2022-05-13 16:14:03 +0530

    Add files via upload

---
+++ Nakkala Kiran/Day-34/angular1.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Angular1Component } from './angular1.component';
+
+describe('Angular1Component', () => {
+  let component: Angular1Component;
+  let fixture: ComponentFixture<Angular1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Angular1Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Angular1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
